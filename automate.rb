0f2a4cf72d9078134a733957c53c5eb624437c4b
Nicolas <nicolas111boivin@outlook.fr>
2020-01-31 19:07:47 +0100

    check pizza exist

---
+++ automate.rb
@@ -0,0 +1,17 @@
+puts 'bonjonr je suis '
+puts' vour voule qelle pizza'
+nom = gets
+  if nom == Anchois&&Andouille&&Champignon
+  puts 'vour voule une'
+  puts nom
+  oui = gets
+  if oui  == "oui"
+    puts nom
+    puts 'quelle Ingredients voule vour'
+    Ingredients = gets
+    puts Ingredients
+
+    puts" vourier vour din pasiente
+    vortre pizza et prete"
+    end
+end
